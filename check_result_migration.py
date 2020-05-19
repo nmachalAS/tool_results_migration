77d043e96747260a4530c1fb2868ae7798eee28e
nmachalAS <n.machal@re-think.consulting>
2020-05-19 14:17:33 +0200

    changes

---
+++ check_result_migration.py
@@ -0,0 +1,358 @@
+import os
+import json
+import sys
+from collections import OrderedDict
+from operator import itemgetter
+import erreur
+import re
+
+
+"""
+Check the results from immigration from allure-results.json files. 
+Take as input the repository that contains all allure-results folders.
+The allure-results folders need to be in the same directory. 
+The name of all allure-results folders need to contains "allure-results".
+Example of names :
+    -"allure-results_1" : directory for results for batch 1
+    -"allure-results_2" : directory for results for batch 2
+    .......
+
+
+There is 1 folder as output named Results_Jobs that contains 6 files :
+    -Results_Job/global report of errors.txt is a file that reports the numbers and kinds of errors that occured.
+    -Results_Job/context_missing_with_users.txt identify all missing contexts matching with the users.
+    -Results_Job/unique_context_missing.txt lists all unique missing contexts
+    -Results_Job/length_element_bigger.txt reports users with failed error : Schema validation error\n[Error] Element length must be lower or equal to 50!
+    -Results_Job/user_missing.txt reports users with failed error : User doesnt exist.
+    -Results_Job/others_errors.txt reports all others issues matching with the users.
+
+usage: python check_results_migration.py <folder with all allure_results directory>
+
+"""
+dictMapErrors={}
+def createErrorsInstancesFromJson():
+    json_to_read="errors.json"
+    global dictMapErrors
+
+    with open(json_to_read) as file_to_read:
+        data = json.load(file_to_read)
+    for error in data:
+        name_dict=error["full_name"].replace(" ","_")
+        dictMapErrors[name_dict]=erreur.Erreur(error)
+
+def getTestsUsers():
+    global dictMapErrors
+    list_tests={}
+    for error in dictMapErrors:
+        if (dictMapErrors[error].type_erreur!="[USER DIFFERENCES]" and dictMapErrors[error].type_erreur!="User not migrated correctly" 
+            and dictMapErrors[error].type_erreur not in list_tests.keys()):
+
+            list_tests[(dictMapErrors[error].type_erreur)]=error
+
+    return list_tests
+
+def listAllAllureResultsFolder(path_allure_results):
+    if path_allure_results[-1]!="/":
+        path_allure_results+="/"
+    list_allure_results_folders = [path_allure_results+ f+"/" for f in os.listdir(path_allure_results) if (os.path.isdir(path_allure_results+ f) and "allure-results" in f)]
+    return list_allure_results_folders
+
+def createFolderForCheckingResults():
+    path_to_past_results="Results_Job/"
+    try:
+        os.mkdir(path_to_past_results)
+    except OSError:
+        print ("Creation of the directory %s failed, it probably already exists." % path_to_past_results)
+    else:
+        print ("Successfully created the directory Results_Job for results %s " % path_to_past_results)
+    return path_to_past_results
+    
+def findTypeContext(context):
+    context_number=re.sub("[^0-9]", "", context)
+    if int(context_number)>999:
+        return "partners"
+    else:
+        return "importers"
+
+def treatRolesDifferent(message,user_id):
+    global dictMapErrors
+    dict_context_roles_users=dictMapErrors["Roles_are_different"].object          
+    all_contexts_changes=message.split("Roles are different")[1:]
+    if "total_added_admin_roles" not in dict_context_roles_users.keys():
+        dict_context_roles_users["total_added_admin_roles"]=0
+        dict_context_roles_users["total_removed_admin_roles"]=0
+        dict_context_roles_users["total_duplicated_admin_roles"]=0
+    for context_changed in all_contexts_changes:
+
+        list_added=[]
+        list_removed=[]
+        list_duplicate=[]
+        context=context_changed.split("in ")[1].split(" New")[0]
+        roles_admin_partners=(1,22,27)
+        roles_admin_importers=(1,22,2)
+        type_context=findTypeContext(context)
+        if type_context=="partners":
+            roles_admin=roles_admin_partners
+        else:
+            roles_admin=roles_admin_importers
+        
+
+        new_old=context_changed.split("Old",1)
+
+        new=new_old[0].split("New ")[1][2:-3]
+        old=new_old[1].split("\n",1)[0].replace(" ", "")[1:-1]
+        list_new=new.split(",")
+        list_old=old.split(",")
+        for new in list_new:
+            try:
+                list_new[list_new.index(new)]=int(new)
+            except:
+                list_new=[]
+        for old in list_old:
+            try:
+                list_old[list_old.index(old)]=int(old)
+            except:
+                list_old=[]
+        for new in list_new:
+            if new not in list_old and new in roles_admin:
+                list_added.append(new)
+                dict_context_roles_users["total_added_admin_roles"]+=1
+                
+                
+        for old in list_old:
+            if old not in list_new and old in roles_admin:
+                list_removed.append(old)
+                dict_context_roles_users["total_removed_admin_roles"]+=1
+            if list_new.count(old)>1 and old in roles_admin:
+                list_duplicate.append(old)
+                dict_context_roles_users["total_duplicated_admin_roles"]+=1
+        
+        if list_duplicate!=[] or list_removed!=[] or list_added!=[]:
+
+            difference="+ : " + str(list_added)+ " ; - : "+str(list_removed)+" ; duplicate : "+str(list_duplicate)
+
+            if context not in dict_context_roles_users.keys():
+                dict_context_roles_users[context]={}
+                dict_context_roles_users[context]["differences"]=OrderedDict()
+                dict_context_roles_users[context]["total_change_for_context"]=0    
+            if difference not in dict_context_roles_users[context]["differences"].keys():
+                dict_context_roles_users[context]["differences"][difference]=OrderedDict()
+                dict_context_roles_users[context]["differences"][difference]["number of users with this context with this difference"]=0
+                dict_context_roles_users[context]["differences"][difference]["user"]=[]
+                
+            dict_context_roles_users[context]["differences"][difference]["number of users with this context with this difference"]+=1
+            dict_context_roles_users[context]["differences"][difference]["user"].append(user_id)
+            dict_context_roles_users[context]["total_change_for_context"]+=1
+
+    dictMapErrors["Roles_are_different"].setObject(dict_context_roles_users)
+
+
+def treatOrgaContextChanged(message,user_id):
+    global dictMapErrors
+    orga_context_added=dictMapErrors["Organizational_Context_added"].object
+    orga_context_removed=dictMapErrors["Organizational_Context_removed"].object
+    all_contexts_changed=message.split("Organizational Context")[1:]
+    for context_changed in all_contexts_changed:
+        action=context_changed.split("[")[0].replace(" ","")
+        context=context_changed.split("[")[1].split("]",1)[0]
+        if action=="added":
+            dictionnaryToUse=orga_context_added
+        else:
+            dictionnaryToUse=orga_context_removed
+        if context not in dictionnaryToUse.keys():
+            dictionnaryToUse[context]={}
+            dictionnaryToUse[context]["nb users"]=0
+            dictionnaryToUse[context]["users"]=[]
+        dictionnaryToUse[context]["nb users"]+=1
+        dictionnaryToUse[context]["users"].append(user_id)
+        if action=="added":
+            orga_context_added=dictionnaryToUse
+        else:
+            orga_context_removed=dictionnaryToUse
+    dictMapErrors["Organizational_Context_added"].setObject(orga_context_added)   
+    dictMapErrors["Organizational_Context_removed"].setObject(orga_context_removed)
+    
+
+def getIdFromJson(data):
+    name=data["name"]
+    if 'UserFileDifference_test' in name :
+        id=name.split("| ",1)[1].split("]",1)[0]
+    elif 'ThirdPartyOrgsFileDifference_test' in name:
+        id=name.split("[",1)[1].split("]",1)[0]
+    elif 'UserMigration_test' in name:
+        id=data["steps"][0]["name"].split("[",1)[1].split(",",1)[0]
+        if len(id)>10:
+            try:
+                id=data["steps"][0]["name"].split("[",1)[1].split(",",1)[1][1:-1]
+            except:
+                id=data["steps"][0]["name"].split("[",1)[1].split(",",1)[0][:-1]
+    elif 'ThirdPartyOrg_test' in name:
+        id=data["steps"][0]["name"].split(": ",1)[1]
+    else:
+        id=name
+    return id
+
+def IsKnownUserDiffError(message):
+    if ("Organizational Context added" in message or "Organizational Context removed" in message 
+        or "Communication Channel" in message or "First Name is incorrect" in message or "Last Name is incorrect" in message
+        or "Roles are different" in message):
+        return True
+    else:
+        return False
+
+def isKnownError(message):
+    listError=["User does not exist","Schema validation error\n[Error] Element length must be lower or equal to 50!",
+        "[USER DIFFERENCES]","Context","[NEW USER]","[REMOVED USER]","[NEW ORGANIZATION]","[REMOVED ORGANIZATION]"]
+    for error in listError:
+        if error in message:
+            return True
+    else:
+        return False
+
+
+def treatUserNotMigratedCorrectly(message,total_of_failures_missing_context,other_errors,user_id):
+    global dictMapErrors
+    dict_context_missing=dictMapErrors["Contexts_are_missing"].object
+    if "Context" in message and "not exists" in message:
+        list_contexts=message.split("Context")[1:]
+        for context in list_contexts:
+            context = context.split("'")[1]
+            if context not in dict_context_missing.keys():
+                dict_context_missing[context]=OrderedDict()
+                dict_context_missing[context]["total_users"]=0
+                dict_context_missing[context]["user"]=[]
+            dict_context_missing[context]["user"].append(user_id)
+            dict_context_missing[context]["total_users"]+=1
+            total_of_failures_missing_context+=1
+    else:
+        other_errors[user_id]=message
+    dictMapErrors["Contexts_are_missing"].setObject(dict_context_missing)
+    return total_of_failures_missing_context,other_errors
+
+def treatUserDifferences(message,user_id,other_errors):
+    global dictMapErrors
+    if IsKnownUserDiffError(message):
+        if "Organizational Context added" in message or "Organizational Context removed" in message:
+            treatOrgaContextChanged(message,user_id)
+        if ("Communication Channel" in message or "First Name is incorrect" in message or "Last Name is incorrect" in message):
+            dictMapErrors["User_data_changed"].object.append(user_id)
+        if "Roles are different" in message:
+            treatRolesDifferent(message,user_id)
+    else:
+        other_errors[user_id]=message
+    
+    return other_errors
+
+
+def treat_json_file_results(json_file,total_of_failures_missing_context,countfailed,list_test_users):
+    global dictMapErrors
+    other_errors={}
+    with open(json_file) as file_to_read:
+        data = json.load(file_to_read)
+        if type(data) is list:
+            print (json_file +" is not a result from migration")
+        elif "status" in data.keys() and data["status"]=="failed":
+            countfailed+=1
+            user_id=getIdFromJson(data)
+            message=data["statusDetails"]["message"]
+            if isKnownError(message):
+                for test in list_test_users:
+                    if test==message:
+                        dictMapErrors[list_test_users[test]].object.append(user_id)
+                if "[USER DIFFERENCES]" in message:
+                    other_errors=treatUserDifferences(message,user_id,other_errors)
+                if "User not migrated correctly" in message:
+                    total_of_failures_missing_context,other_errors=treatUserNotMigratedCorrectly(message,total_of_failures_missing_context,other_errors,user_id)
+            else:
+                other_errors[user_id]=message
+
+
+
+
+    return total_of_failures_missing_context,countfailed,other_errors
+
+def countandCreateErrorsMessages(total_of_failures_missing_context,countfailed,other_errors):
+    global dictMapErrors
+    number_errors=OrderedDict()
+   
+
+    for error in dictMapErrors:
+        dictMapErrors[error].countErrors()
+        dictMapErrors[error].initiateErrorMessage()
+
+    number_errors["other_errors"]=len(other_errors)
+    number_errors["failures_missing_context"]=total_of_failures_missing_context
+    number_errors["total_users_errors"]=countfailed
+
+    error_messages=[]
+    error_messages.append(str(number_errors["total_users_errors"])+" users have at least 1 error")
+    error_messages.append(str(number_errors["failures_missing_context"])+" errors with missing contexts")
+    error_messages.append(str(number_errors["other_errors"])+" other errors")
+    return number_errors,error_messages
+
+def printAndSaveReportErrorMessages(path_to_past_results,error_messages):
+    global dictMapErrors
+    with open(path_to_past_results+'global report of errors','w') as f:
+        for error in dictMapErrors:
+            f.write("%s\n" % dictMapErrors[error].exit_message)
+            print(dictMapErrors[error].exit_message)
+        for error in error_messages:
+            f.write("%s\n" % error)
+            print(error)
+    print("Check Results_Job folders to see results in details")
+
+def createFilesResults(path_to_past_results,error_messages,number_errors,other_errors):
+    printAndSaveReportErrorMessages(path_to_past_results,error_messages)
+    createFilesResultsByErrorTypes(path_to_past_results,number_errors,other_errors)
+
+def sortDictionnaryForJson():
+    global dictMapErrors
+    for error in dictMapErrors:
+        dictMapErrors[error].sortObject()
+
+
+def createFilesResultsByErrorTypes(path_to_past_results,number_errors,other_errors):
+    global dictMapErrors
+
+    sortDictionnaryForJson()
+    for error in dictMapErrors:
+        dictMapErrors[error].createFilesResults()
+
+    
+    with open(path_to_past_results+'others_errors.json', 'w') as fp:
+        fp.write("%s" % str(number_errors["other_errors"])+" errors. \nList of all others errors\n")
+        json.dump(other_errors, fp,indent=4, separators=(',', ': '),ensure_ascii=True)
+
+
+
+def ProcessCheckResults(path_allure_results):
+    createErrorsInstancesFromJson()
+    list_test_users=getTestsUsers()
+    list_allure_results_folders = listAllAllureResultsFolder(path_allure_results)
+    path_to_past_results=createFolderForCheckingResults()
+    
+    total_of_failures_missing_context=0
+    countfailed=0
+    for allure_results_folder in list_allure_results_folders:
+
+        list_json_files = [allure_results_folder+ f for f in os.listdir(allure_results_folder) if ".json" in f]
+
+        for json_file in list_json_files:
+
+            total_of_failures_missing_context,countfailed,other_errors=treat_json_file_results(json_file,total_of_failures_missing_context,countfailed,list_test_users)
+
+    
+    number_errors,error_messages=countandCreateErrorsMessages(total_of_failures_missing_context,countfailed,other_errors)
+
+
+
+    createFilesResults(path_to_past_results,error_messages,number_errors,other_errors)
+
+"""
+if len(sys.argv) < 2:
+    print("Check results errors from migration with json files in allure-results folders.\nusage: python check_results_migration.py <folder with all allure_results directory>")
+    sys.exit(1)
+
+path_allure_results=sys.argv[1]"""
+path_allure_results="Migration/Results2"
+ProcessCheckResults(path_allure_results)
--- erreur.py
+++ erreur.py
@@ -1,3 +1,7 @@
+from collections import OrderedDict
+from operator import itemgetter
+import json
+
 class Erreur:
     """Classe définissant une erreur caractérisée par :
     - son type
@@ -9,12 +13,71 @@ class Erreur:
     - Le Head du message de sortie"""
 
     
-    def __init__(self, type_erreur, identify,type_file,full_name_error,exit_message,head_file):
-        """Constructeur de notre classe"""
-        self.type_erreur = type_erreur
-        self.identify = identify
-        self.type_file = type_file
+    def __init__(self, data_json):
+
+        self.type_erreur = data_json["type"]
+        self.name=data_json["full_name"]
+        self.expression_to_check=data_json["expression_to_look"]
+        self.identify = data_json["identify"]
+        self.type_file = data_json["type_exit_file"]
+        self.name_exit_file=data_json["full_name"]+data_json["type_exit_file"]
         self.nb_affected = 0
-        self.full_name_error=full_name_error
-        self.exit_message=exit_message
-        self.head_file=head_file
+        self.exit_message=data_json["message_exit_file"]
+        self.head_file=data_json["message_exit_file"]
+        self.object=self.findTypeObect()
+        self.path_to_past_results="Results_Job/"
+    
+    def findTypeObect(self):
+        if self.identify=="users":
+            return []
+        else:
+            return OrderedDict()
+    
+    def setObject(self,newObject):
+        self.object=newObject
+    
+    def countErrors(self):
+        self.nb_affected = len(self.object)
+    
+    def initiateErrorMessage(self):
+        self.exit_message=str(self.nb_affected)+self.exit_message
+
+    def sortObject(self):
+        if self.name=="Contexts are missing" or self.name=="Roles are different" or self.name=="Organizational Context added" or self.name=="Organizational Context removed":
+            if self.name=="Contexts are missing":
+                    key_order = ('context', 'total_users', 'user')
+                    itemToSort='total_users'
+            elif self.name=="Roles are different":
+                key_order=("context","total_change_for_context","differences")
+                itemToSort='total_change_for_context'
+            elif self.name=="Organizational Context added" or self.name=="Organizational Context removed":
+                key_order=("context","nb users","users")
+                itemToSort='nb users'
+            current_error=self.object
+            list_error=[]
+            for dictionnary in current_error:
+                if dictionnary!="total_added_admin_roles" and dictionnary!="total_removed_admin_roles" and dictionnary!="total_duplicated_admin_roles":
+                    current_error[dictionnary]["context"]=dictionnary
+                    new_queue = OrderedDict()
+                    for k in key_order:
+                        new_queue[k] = current_error[dictionnary][k]
+                    current_error[dictionnary]=new_queue
+                    list_error.append(current_error[dictionnary])
+     
+            list_error=sorted(list_error, key=itemgetter(itemToSort),reverse=True)
+            if self.name=="Roles are different":
+                list_error.insert(0,current_error["total_added_admin_roles"])
+                list_error.insert(0,current_error["total_removed_admin_roles"])
+                list_error.insert(0,current_error["total_duplicated_admin_roles"])
+
+            self.object=list_error
+    
+    def createFilesResults(self):
+        with open(self.path_to_past_results+self.name_exit_file,'w') as f:
+            f.write("%s" % self.exit_message)
+            if self.type_file==".json":
+                json.dump(self.object, f,indent=4, separators=(',', ': '),ensure_ascii=True)
+            else:
+                for item in self.object:
+                    f.write("%s\n" % item)
+        
